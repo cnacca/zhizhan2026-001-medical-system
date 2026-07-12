cbb3d919a2be1a6d0a42520972132e99fe4a4fba
cnacca <2397402725@qq.com>
2026-07-13 05:16:51 +0800

    feat: clone remaining doctor portal pages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,6 @@ import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
+import './doctor-portal.css'
 
 createApp(App).use(ElementPlus).mount('#app')
